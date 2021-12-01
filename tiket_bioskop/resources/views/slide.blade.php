0ade89a4c82b456a266b6a6da8fd2b32012130b2
gustirhy <90498455+gustirhy@users.noreply.github.com>
2021-11-30 19:55:30 -0800

    slide tiket_bioskop team 1

---
+++ tiket_bioskop/resources/views/slide.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style-slide.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<body>
+<div class=tiket_bioskop-slider>
+		<div class=isi-slider>
+			<img src="h1.JPG" alt="Gambar 1">
+			<img src="h2.JPG" alt="Gambar 2">
+			<img src="h3.JPG" alt="Gambar 3">
+		</div>
+	</div>
+</body>
+<header>
+<div class="bg">
+  <div id="filmbioskop">
+  	<h1>film bioskop</h1>
+    </div>
+    <nav>
+    <ul>
+        <li><a class="active" href="/">Slide Film</a></li>
+        </li>
+      <li><a class="beranda" href="/">Beranda</a></li>
+      </li>
+       <li><a class="film" href="{{url('film')}}">Film</a></li>
+      </li>
+<li class="kontak"><a href="#">Kontak</a></li>
+      </li>
+</div>
+</html>
